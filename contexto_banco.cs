ef982974660af27cc422e19cba90fcd125b5f9a2
“Saymon <srrasador@ucs.br>
2024-12-05 21:01:40 -0300

    commit inicial

---
+++ contexto_banco.cs
@@ -0,0 +1,28 @@
+// Nesse arquivo é criado a classe ContextoBancoDados no qual dentro dela é mencionado as tabelas existentes
+
+using Microsoft.EntityFrameworkCore;
+
+public class ContextoBancoDados : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+    }
+
+    // Exemplo de criação de tabela chamada "Cliente"
+    // public DbSet<Cliente> clientes { get; set; }
+
+    // após, crie um arquivo tabelas/clientes.cs
+    // nesse arquivo clientes.cs voce cria de fato a tabela
+
+    // public class Cliente
+    // {
+    // public int Id { get; set; }
+    // public string Nome { get; set; }
+    // public string Email { get; set; }
+    // public DateTime DataCadastro { get; set; }
+    // }
+
+
+}
